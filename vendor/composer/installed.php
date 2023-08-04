1fcc8d272780d6ea369faa59818ff6c209c636be
Mounir-Bmz <mboumaza.mounir@gmail.com>
2023-08-04 18:24:56 +0200

    Correction gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'student/s05-exo-pokedex-mounir-bmz',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '104ca1c143d16e25dc51c070dcfe1f437dc1559c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'altorouter/altorouter' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '39c50092470128c12284d332bb57f306bb5b58e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'student/s05-exo-pokedex-mounir-bmz' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '104ca1c143d16e25dc51c070dcfe1f437dc1559c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
